f011746d86222fd48a1b11481b68f55b1222fafb
soujanyavatt <84461100+soujanyavatt@users.noreply.github.com>
2021-12-06 14:01:35 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,43 @@
+import streamlit as st
+
+st.header("Soujanya Vattikolla")
+
+with st.expander("Career Objective"):
+    st.write("""
+         Desirous of an IT Service Management role in a challenging workplace that welcomes innovative ideas, offers growth opportunities, and positive environment for accomplishing projects.
+         """)
+
+with st.expander("Education Details"):
+    st.subheader(""" Gayathri Vidya Parishad College of Engineering""")
+    st.write(""" Bachelor Of Technology (COMPUTER SCIENCE ENGINEERING) """)
+    st.write(""" 66% """)
+    st.subheader(""" Vikas Mahila Junior College """)
+    st.write(""" Intermediate ( A.P Board) """)
+    st.write(""" 88.5% """)
+    st.subheader(""" MVP Public School """)
+    st.write(""" I.C.S.E """)
+    st.write(""" 68.3% """)
+
+with st.expander("Technical Skills"):
+    st.subheader("Languages")
+    st.write(" Python, Machine Learning, Deep Learning and SQL")
+
+with st.expander("Career Contour"):
+    st.write("* Data Science Internship at Innomatics Research Labs from April to till date.")
+    st.write("* Worked as a Service executive for HSBC Technology and Services. ")
+
+with st.expander("Projects"):
+    st.subheader("Budget Friendly Hotels (Web Scraping project)")
+    st.write("Technology used: Python ")
+    st.subheader("Estimation of Concrete Compressive Strength (Machine Learning project) ")
+    st.write("Technology used: Python + Machine Learning Regression Models")
+    st.subheader("Health Insurance Lead Prediction (Machine Learning project) ")
+    st.write("Technology used: Python + Machine Learning Classification Models ")
+    st.subheader("URL Shortener Web Application")
+    st.write("Technology used: Python + Flask + Sqlite3 + SQLAlchemy Database + HTML")
+
+with st.expander("Contact Details"):
+    st.write("soujanyanaidu25@gmail.com")
+
+
+    
